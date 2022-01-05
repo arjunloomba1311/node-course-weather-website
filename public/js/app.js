ffd647edd0f443df3b95ac3ee299ed8f7cf8668e
arjunloomba1311 <arjunrajloomba@gmail.com>
2022-01-05 15:15:45 +0530

    remove unneccessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("hello js client is loaded")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
